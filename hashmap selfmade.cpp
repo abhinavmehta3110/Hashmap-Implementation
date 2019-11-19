58abd6d0a1bd1157b68cddd50bc22168eaad2944
abhinavmehta3110 <43959164+abhinavmehta3110@users.noreply.github.com>
2019-11-20 01:31:32 +0530

    Update hashmap selfmade.cpp

--- hashmap selfmade.cpp
+++ hashmap selfmade.cpp
@@ -8,12 +8,12 @@ class mapNode
 	public:
 	string key;
 	V value;
-	mapNode<V> * next;
+	mapNode<V>* next;
 	
-	mapNode( string key , V value )
+	mapNode(string key, V value)
 	{
-		this -> value = value;
-		this -> key = key;
+		this->value = value;
+		this->key = key;
 		next = NULL;
 	}
 	~mapNode()
@@ -27,14 +27,14 @@ template<typename V>
 class ourmap
 {
 	public:
-	mapNode<V> ** bucket;
+	mapNode<V>** bucket;
 	int count;
 	int numBuckets;
 	
 	ourmap()
 	{
 		bucket = new mapNode<V>*[5];
-		for( int i = 0 ; i < 5 ; i++ )
+		for(int i = 0; i < 5; i++)
 		{
 			bucket[i] = NULL;
 		}
@@ -55,16 +55,16 @@ class ourmap
 	}
 	
 	private:
-	int hash( string key )
+	int hash(string key)
 	{
-		int ans = 0; // ans denotes hashcode
+		int ans = 0; 			// ans denotes hashcode
 		int len = key.length();
 		int multiplier = 1;
 		for( int i = len - 1 ; i >= 0 ; i-- )
 		{
 			ans += multiplier * key[i];
 			ans = ans % numBuckets;
-			multiplier *= 37;
+			multiplier *= 37;		//we can use any prime number here at place of 37
 			multiplier = multiplier % numBuckets;
 		}
 		return ans % numBuckets;
@@ -74,19 +74,19 @@ class ourmap
 	{
 		mapNode<V>** temp = bucket;
 		bucket = new mapNode<V>*[2 * numBuckets];
-		for( int i = 0 ; i < 2 * numBuckets ; i++ )
+		for(int i = 0; i < 2 * numBuckets; i++)
 		{
 			bucket[i] = NULL;
 		}
 		int oldNumBuckets = numBuckets;
 		count = 0;
 		numBuckets *= 2;
-		for( int i = 0 ; i < oldNumBuckets ; i++ )
+		for(int i = 0; i < oldNumBuckets; i++)
 		{
 			mapNode<V> * head = temp[i];
-			while( head != NULL )
+			while(head != NULL)
 			{
-				insert( head -> key , head -> value );
+				insert(head -> key , head -> value);
 				head = head -> next;
 			}
 		}
@@ -94,73 +94,73 @@ class ourmap
 	}
 	
 	public:
-	void insert( string key , V value )
+	void insert(string key, V value)
 	{
-		int index = hash( key );
-		mapNode<V> * head = bucket[index];
-		while( head != NULL )
+		int index = hash(key);
+		mapNode<V>* head = bucket[index];
+		while(head != NULL)
 		{
-			if( head -> key == key )
+			if(head->key == key)
 			{
-				head -> value = value;
+				head->value = value;
 				return;
 			}
-			head = head -> next;
+			head = head->next;
 		}
 		head = bucket[index];
-		mapNode<V> * newNode = new mapNode<V>(key , value);
-		newNode -> next = head;
+		mapNode<V>* newNode = new mapNode<V>(key, value);
+		newNode->next = head;
 		bucket[index] = newNode;
 		count++;
 		
 		double loadFactor = (double)count / numBuckets;
-		if( loadFactor > 0.7 )
+		if(loadFactor > 0.7)
 		{
 			rehash();
 		}
 	}
 	
-	V find( string key )
+	V find(string key)
 	{
-		int index = hash( key );
-		mapNode<V> * head = bucket[index];
-		while( head != NULL )
+		int index = hash(key);
+		mapNode<V>* head = bucket[index];
+		while(head != NULL)
 		{
-			if( head -> key == key )
+			if(head->key == key)
 			{
-				return head -> value;
+				return head->value;
 			}
-			head = head -> next;
+			head = head->next;
 		}
 		return 0;
 	}
 	
-	V remove( string key )
+	V remove(string key)
 	{
-		int index = hash( key );
+		int index = hash(key);
 		mapNode<V> * head = bucket[index];
 		mapNode<V> * prev = NULL;
-		while( head != NULL )
+		while(head != NULL)
 		{
-			if( head -> key == key )
+			if(head->key == key)
 			{
-				if( prev == NULL )
+				if(prev == NULL)
 				{
-					bucket[index] = head -> next;
+					bucket[index] = head->next;
 				}
 				else
 				{
-					prev -> next = head -> next;
+					prev->next = head->next;
 				}
 				
-				head -> next = NULL;
-				V ans = head -> value;
+				head->next = NULL;
+				V ans = head->value;
 				delete head;
 				count--;
 				return ans;
 			}
 			prev = head;
-			head = head -> next;
+			head = head->next;
 		}
 		return 0;
 	}
@@ -176,24 +176,24 @@ int main()
 {
 	ourmap<int> m;
 	string s = "abc";
-	for( int i = 0 ; i < 10 ; i++ )
+	for(int i = 0; i < 10; i++)
 	{
 		string s1 = s + char(i + '0');
-		m.insert( s1 , i + 1 );
+		m.insert(s1, i + 1);
 		cout << m.getLoadFactor() << endl;
 	}
 	cout << m.getSize() << endl;
 	m.remove("abc1");
 	m.remove("abc4");
 	cout << m.getSize() << endl;
-	for( int i = 0 ; i < 10 ; i++ )
+	for(int i = 0; i < 10; i++)
 	{
 		string s1 = s + char(i + '0');
 		cout << s1 << " " << m.find(s1) << endl;
 	}
-	m.insert("abc0" , 5);
+	m.insert("abc0", 5);
 	cout << m.getSize() << endl;
-	m.insert( "abc34" , 35) ;
+	m.insert("abc34", 35) ;
 	cout << m.getSize() << endl; 
 	
 }
